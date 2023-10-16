f9895ad44808a54a97b375fe8a9bdf5999487797
Camilo <camiloquea98@gmail.com>
2023-10-16 13:20:30 -0500

    client side server example

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { api } from "@/lib/trpc/react-provider";
 import { RouterOutputs } from "@/lib/trpc/shared";
 import React from "react";
 
@@ -8,12 +9,16 @@ export const TodoList = ({
 }: {
   initialTodos?: RouterOutputs["getTodos"];
 }) => {
+  const { data, refetch, isFetching } = api.getTodos.useQuery(undefined, {
+    initialData: initialTodos,
+    enabled: false,
+  });
   return (
     <div>
       <h1>TODO</h1>
-      {/* <button onClick={() => refetch()}>Refetch</button>
-      <div>{isFetching ? "Loading..." : null}</div> */}
-      <pre>{JSON.stringify(initialTodos, undefined, 4)}</pre>
+      <button onClick={() => refetch()}>Refetch</button>
+      <div>{isFetching ? "Loading..." : null}</div>
+      <pre>{JSON.stringify(data, undefined, 4)}</pre>
     </div>
   );
 };
