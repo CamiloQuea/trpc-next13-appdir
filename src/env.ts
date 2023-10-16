72425029c9e8a89f126470465f889600aa10556b
Camilo <camiloquea98@gmail.com>
2023-10-16 13:18:36 -0500

    dotenv removed

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,10 @@
 import { z } from 'zod'
-import dotenv from 'dotenv'
-dotenv.config()
 
 const myEnv = z.object({
     NODE_ENV: z
         .enum(['development', 'test', 'production'])
         .optional()
         .default('development'),
-
 })
 
 const env = myEnv.parse(process.env)
